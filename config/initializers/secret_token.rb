b2159808b023db4f44115f3121ad9753eef38e9f
Alex Ianus <hire@alexianus.com>
2014-05-10 11:49:58 +0700

    Initial commit of fresh Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniauthCoinbaseExample::Application.config.secret_key_base = '593c1c53b465930f572df18ffde2815f83ba3a9fb6c43ce462410e54569136498f1e41964bbec4c4a4d9a4f3fc781f891bb60325a9f7e0281df4442c9bb8b82d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OmniauthCoinbaseExample::Application.config.session_store :cookie_store, key: '_omniauth-coinbase-example_session'
